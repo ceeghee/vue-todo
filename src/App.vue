6fdf02d10f2961f4b00eccbd14f4695044455d0c
Goodluck Ekene <goodluck.ekene@gmail.com>
2020-05-28 01:27:23 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import sweetalert from 'sweetalert';
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+   components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+		todos: [{
+			title: 'Todo A',
+			project: 'Project A',
+			done: false,
+			}, {
+			title: 'Todo B',
+			project: 'Project B',
+			done: true,
+			}, {
+			title: 'Todo C',
+			project: 'Project C',
+			done: false,
+			}, {
+			title: 'Todo D',
+			project: 'Project D',
+			done: false,
+		}],
+    };
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
 }
 </script>
 
